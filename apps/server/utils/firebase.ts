7a652ee5381300b1ce1b8a5448a8a799c102c0c6
naiyar2000 <naiyarimam.2000@gmail.com>
2025-07-07 23:21:11 +0530

    json encoding fix

--- apps/server/utils/firebase.ts
+++ apps/server/utils/firebase.ts
@@ -1,14 +1,14 @@
 import admin from "firebase-admin";
 
 //for json file(uncomment below line to running locally)
-// import serviceAccount from '../firebase-service-account.json'
+import serviceAccount from '../firebase-service-account.json'
 
 
 //for utf8 encoded file(for storing and retrieving config json from github secrets)
-import fs from 'fs';
-const serviceAccount = JSON.parse(
-  fs.readFileSync('../firebase-service-account.json', 'utf8')
-);
+// import fs from 'fs';
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync('../firebase-service-account.json', 'utf8')
+// );
 
 
 if (!admin.apps.length) {
