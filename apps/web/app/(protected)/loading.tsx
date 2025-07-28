369e6b0ffb9c53150f6b8beca0ed351e4cdededf
naiyar2000 <naiyarimam.2000@gmail.com>
2025-07-28 22:43:29 +0530

    included next.js default loading for protected routes and settings page

---
+++ apps/web/app/(protected)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoadingPage from '../loading'
+
+const loading = () => {
+    return (
+        <LoadingPage title='Worth the wait...' />
+    )
+}
+
+export default loading
---
+++ apps/web/app/(protected)/settings/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingPage from '@/app/loading'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <LoadingPage title='Fetching settings...'/>
+  )
+}
+
+export default loading
