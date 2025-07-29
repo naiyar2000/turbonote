9e13a5ec459e0668426872b8fd74e1660f10355f
naiyar2000 <naiyarimam.2000@gmail.com>
2025-07-29 23:52:10 +0530

    profile loading screen

---
+++ apps/web/app/(protected)/profile/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingPage from '@/app/loading'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <LoadingPage title='Fetching profile details...'/>
+  )
+}
+
+export default loading
