b38ac02b499f2a796145b47aadf11fe9008b004a
naiyar2000 <naiyarimam.2000@gmail.com>
2025-07-30 23:38:45 +0530

    workspace loader

---
+++ apps/web/app/(protected)/my-workspace/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingPage from '@/app/loading'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <LoadingPage title='Loading workspace...'/>
+  )
+}
+
+export default loading
